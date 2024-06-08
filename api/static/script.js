d4632c286968b90c8569b088acf012f4ab4e8fa5
moviea849w <164612842+moviea849w@users.noreply.github.com>
2024-06-08 13:46:36 +0530

    Add files via upload

---
+++ api/static/script.js
@@ -0,0 +1,16 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const listItems = document.querySelectorAll("#listOfServers li");
+
+    listItems.forEach(item => {
+        const link = item.querySelector("a");
+        const href = link.getAttribute("href").trim();
+
+        if (href === "" || href === "#") {
+            item.remove();
+        }
+    });
+}); 
+
